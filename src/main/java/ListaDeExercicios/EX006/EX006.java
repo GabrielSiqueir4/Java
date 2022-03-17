ee94bf2a8c7e09c3e7baed0fa40bba4569c5fcc5
Gabriel Da Silva Siqueira <99335926+GabrielSiqueir4@users.noreply.github.com>
2022-03-17 09:46:37 -0300

    Exercicio6

---
+++ src/main/java/ListaDeExercicios/EX006/EX006.java
@@ -0,0 +1,35 @@
+package ListaDeExercicios.EX006;
+
+import java.util.Scanner;
+
+public class EX006 {
+    public static void main(String[] args) {
+        Scanner numero = new Scanner(System.in);
+
+
+        System.out.println("informe o primeiro numero");
+        double primeiro = numero.nextDouble();
+        System.out.println("informe o segundo numero");
+        double segundo = numero.nextDouble();
+        System.out.println("informe o terceiro numero");
+        double terceiro = numero.nextDouble();
+
+        if (primeiro > segundo && primeiro > terceiro) {
+            System.out.println("o numero Maior é o primerio");
+        } else if (primeiro == segundo || primeiro == terceiro) {
+            System.out.println("numeros iguais");
+        }
+        if (segundo > primeiro && segundo > terceiro) {
+            System.out.println("o numero maior é o segundo");
+        } else if (segundo == primeiro || segundo == terceiro) {
+            System.out.println("numeros iguais");
+        }
+        if (terceiro > primeiro && terceiro > segundo) {
+            System.out.println("o terceiro numero é o maior");
+        } else if (terceiro == primeiro || terceiro == segundo) {
+            System.out.println("numero iguais ");
+        }
+
+
+    }
+}
