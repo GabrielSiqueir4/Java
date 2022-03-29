fe83b6d17b171851026a6cd603191672c6caf971
Gabriel Da Silva Siqueira <99335926+GabrielSiqueir4@users.noreply.github.com>
2022-03-28 22:41:35 -0300

    teste

--- ListaDeExercicios/src/main/java/ListaVetorLuan/Exericios/Exercicio6.java
+++ ListaDeExercicios/src/main/java/ListaVetorLuan/Exericios/Exercicio6.java
@@ -3,7 +3,7 @@ package Exericios;
 /*
 * 6.	Criar dois vetores A e B cada um com 10 elementos inteiros. Construir um vetor C, onde cada elemento de C é a subtração dos respectivos elementos em A e B, ou seja: C[i] = A[i] – B[i]. */
 
-import com.sun.jdi.PathSearchingVirtualMachine;
+
 
 public class Exercicio6 {
 
